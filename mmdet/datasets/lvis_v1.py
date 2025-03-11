aa2a41ef86aa1ea904baaef12d0c3a2a50294ec3
Vu Tran <vuptran@gmail.com>
2025-03-11 00:45:58 +0000

    Fix typo

--- mmdet/datasets/lvis_v1.py
+++ mmdet/datasets/lvis_v1.py
@@ -41,7 +41,7 @@ class LVISV1Dataset(BaseLVISV1Dataset):
             })
             data_list.append(parsed_data_info)
         if self.ANN_ID_UNIQUE:
-            assert len(set(total_ann_ids)) == len(total_ann_ids),
+            assert len(set(total_ann_ids)) == len(total_ann_ids), \
             f"Annotation ids in ``{self.ann_file}`` are not unique!"
         
         del self.lvis
